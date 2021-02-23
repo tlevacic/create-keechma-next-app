aa97f15dea9b26db0d19d9756dbdb0e01d30ba90
Tin Levacic <tin.levacic@verybigthings.com>
2021-02-23 14:04:53 +0100

    small change

--- src/cli.js
+++ src/cli.js
@@ -10,21 +10,15 @@ function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
       "--git": Boolean,
-      "--yes": Boolean,
-      "--install": Boolean,
       "-g": "--git",
-      "-y": "--yes",
-      "-i": "--install",
     },
     {
       argv: rawArgs.slice(2),
     }
   );
   return {
-    skipPrompts: args["--yes"] || false,
     git: args["--git"] || false,
     projectName: args._[0],
-    runInstall: args["--install"] || false,
   };
 }
 
